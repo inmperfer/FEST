b286ea0988a5f74202d1d252721b90e811a53d2f
Inmaculada Perea Fernandez <inma.perea.fdz@gmail.com>
2017-03-26 12:12:35 +0200

    ejercicios evaluacion

---
+++ paquetesainstalar_alumnos.R
@@ -0,0 +1,31 @@
+# Paquetes a instalar (CRAN)
+# 
+
+install.packages(c('devtools','ggplot2','ggthemes',"stringr","plyr","dplyr",'tidyr',"data.table",'Hmisc'))
+install.packages(c('lubridate','magrittr','pracma','psych','reshape','reshape2','RCurl','xts','zoo'))
+install.packages(c('mvtnorm','randomForest','rpart'))
+install.packages(c('lpSolve','quadprog'))
+install.packages(c("xlsx","XLConnect","openxlsx","readxl","haven","readr","readODS","rio",'rvest'))
+install.packages(c('googleVis','googlesheets'))
+install.packages(c('DBI','RSQLite','sqldf','RODBC','RPostgreSQL'))
+install.packages(c('rmarkdown','knitr','knitrBootstrap','knitcitations','knitLatex','RefManageR','xtable'))
+install.packages(c('ReporteRs','reports','rmdformats','rticles','sjmisc','sjPlot','stargazer','ztable','texreg'))
+install.packages(c('Rcmdr'))
+install.packages(c('twitteR'))
+
+
+install.packages(c('foreach','doParallel'))
+
+install.packages(c("ISLR","AppliedPredictiveModeling","elasticnet","caret","MASS","car","emdbook")) # Juan Luis
+
+install.packages(c('igraph','pyramid','wordcloud'))  # sobre gráficos
+
+install.packages(c('e1071','HSAUR3','learningr','mosaic'))  # De libros  (no: 'EDAWR')
+install.packages(c('hflights'))  # De datos
+
+install.packages(c('DiagrammeR','leaflet','d3Network'))
+install.packages(c('DT'))  # shiny
+
+install.packages(c('shiny'))  # shiny
+
+
